cd134d92a73e4829297733b4c816232e458eb287
Rogelio Sergio Ramirez III <rogelio.ramirez@gmail.com>
2017-09-28 21:18:52 -0500

    changed index.html to index.php

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="A Time of War Character Creator">
+    <meta name="author" content="Rogelio Sergio Ramirez III">
+    <link rel="icon" href="http://bg.battletech.com/bt_icon.png?x64300">
+    <title>Battletech's A Time of War Character Creator</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="styles.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">ATOW</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://bg.battletech.com">Official Battletech Site</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="headline">
+        <h1>Tired of spending hours on Life Paths?</h1>
+        <h2 class="lead">Try out A Time of War web application today!</p>
+        <a class="btn btn-warning" href="http://bg.battletech.com">Try for free!</a>
+      </div>
+
+      <div class="row">
+        <!-- You no longer need to select a number of columns. If you just put 3 columns, bootstrap will auto size -->
+        <div class="col-md">
+          <h3>Life Paths confusing your players?</h2>
+          <p>With A Time of War Character Creator, all the possibiliites are right in front of you.
+            You no longer need to look through many pages of mulitple books to find the Life Path your players want.
+            Instead, it is right on the screen with an easy to follow progression chart.
+          </p>
+          <a class="btn btn-warning" href="#">Live your Life (Path) to the fullest.</a>
+        </div>
+        <div class="col-md">
+           <h3>Loosing track of skillpoints?</h3>
+           <p>The A Tiime of War Character Creator, will always keep track of all the skill points,
+             as you build your character. Options in the program include optimization, skillpoint only builds, 
+             and expirence points until next level.
+           </p>
+           <a class="btn btn-warning" href="#">Let us do the math for you.</a>
+        </div>
+        <div class="col-md">
+           <h3>To many traits and no idea what they do?</h3>
+           <p>With our character creator, you can print out the rules for your character's traits and skills with your
+             character sheet. No more needing to flip pages left and right looking for one specific rule for your trait.
+           </p>
+           <a class="btn btn-warning" href="www.google.com">Print all the Rules!</a>
+        </div>
+      </div> <!-- .row -->
+      <div class="solution">
+        <img src="assets/img/ATOW.jpg">
+        <p>The "A Time of War" Character Creator is a web application that will allow the gamemaster or the players to log on
+         and manage their character(s). The character creator comes allows the GM or players to create their characters using the
+         Point Buy system or the more organic Life Path system. The web application will do all the math in the background as the
+         players create their characters. At the end, they will be able to make any tweaks based on optimization, age or custom rules
+         that the GM will want. 
+        </p>
+        <p>The other feature that will make life much easier for both the GM and the players will be that they will be able to
+          print their character sheets AND any special rules for their skills and traits. This will lead to the game becomeing
+          much more beginner friendly and help promote the game.
+        </p>
+      </div>
+      <div class="pricing">
+        <p>
+          The "A Time of War" Character Creator can be yours for just $5.99 per month. The web application will kepp all of your files ready
+          to be accessed anywhere in the world. 
+        </p>
+        <a class="btn btn-warning">Sign up Today!</a>
+      </div>
+
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+   
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
